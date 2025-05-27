52fdffcb1a7854f50915845731e82e953b475cd0
lalitm1004 <lalitm1004@gmail.com>
2025-05-27 21:25:45 +0530

    24 current  2025-05-27 21:25:43  25.11.20250525.62b852f     6.12.30                          *

--- nixos/hosts/monad/modules/env.nix
+++ nixos/hosts/monad/modules/env.nix
@@ -1,6 +1,13 @@
-{
+{pkgs, ...}: {
   environment.variables = {
     EDITOR = "hx";
     ZDOTDIR = "/home/lalit/.config/zsh";
+
+    # prisma env
+    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+    PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+    PRISMA_MIGRATION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/migration-engine";
+    PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
   };
 }
