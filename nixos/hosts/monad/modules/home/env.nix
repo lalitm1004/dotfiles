9897852b0be031b2a1e5aad08bd5841ec6fcbf3e
lalitm1004 <lalitm1004@gmail.com>
2025-05-28 23:29:27 +0530

    51 current  2025-05-28 23:29:23  25.11.20250525.62b852f  6.12.30                          *

--- nixos/hosts/monad/modules/home/env.nix
+++ nixos/hosts/monad/modules/home/env.nix
@@ -1,7 +1,19 @@
-{
+{ config, ... }: {
   home.file = {
     ".zshenv".text = ''
       export ZDOTDIR="$HOME/.config/zsh"
     '';
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/.home/Desktop";
+    download = "${config.home.homeDirectory}/.home/Downloads";
+    documents = "${config.home.homeDirectory}/.home/Documents";
+    music = "${config.home.homeDirectory}/.home/Music";
+    pictures = "${config.home.homeDirectory}/.home/Pictures";
+    publicShare = "${config.home.homeDirectory}/.home/Public";
+    templates = "${config.home.homeDirectory}/.home/Templates";
+    videos = "${config.home.homeDirectory}/.home/Videos";
   };
 }
