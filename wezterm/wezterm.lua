6fbe9bf97c9a828b7530a74f8538231c4da5394f
lalitm1004 <lalitm1004@gmail.com>
2025-05-31 15:16:11 +0530

    setup ubuntu

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,158 @@
+local wezterm = require("wezterm")
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Maximizing on startup
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+
+-- Keybinds
+config.leader = { key = "`", timeout_milliseconds = 2000 }
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "c",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    mods = "LEADER",
+    key = "x",
+    action = wezterm.action.CloseCurrentPane { confirm = true }
+  },
+  {
+    mods = "LEADER",
+    key = "b",
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    mods = "LEADER",
+    key = "n",
+    action = wezterm.action.ActivateTabRelative(1)
+  },
+  {
+    mods = "LEADER",
+    key = "=",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "-",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action.ActivatePaneDirection "Left"
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action.ActivatePaneDirection "Right"
+  },
+  {
+    mods = "LEADER",
+    key = "LeftArrow",
+    action = wezterm.action.AdjustPaneSize { "Left", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "RightArrow",
+    action = wezterm.action.AdjustPaneSize { "Right", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "DownArrow",
+    action = wezterm.action.AdjustPaneSize { "Down", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "UpArrow",
+    action = wezterm.action.AdjustPaneSize { "Up", 5 }
+  },
+}
+
+-- Map all tabs
+for i = 0, 9 do
+  table.insert(config.keys, {
+    mods = "LEADER",
+    key = tostring(i),
+    action = wezterm.action.ActivateTab(i),
+  })
+end
+
+-- Tab Bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- Tmux Status
+wezterm.on("update-right-status", function(window, _)
+  local SOLID_LEFT_ARROW = ""
+  local ARROW_FOREHEAD = { Foreground = { Color = "#ff0000" } }
+  local prefix = ""
+  local bg_color = "#0a0a0a"
+
+  if window:leader_is_active() then
+    prefix = " " .. utf8.char(0x1f6f8) -- 🛸
+    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  end
+
+  if window:active_tab():tab_id() == 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = bg_color } }
+  end -- arrow color based on if tab is first pane
+
+  window:set_left_status(wezterm.format {
+    { Background = { Color = bg_color } },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW }
+  })
+end)
+
+-- Theming
+config.color_scheme = "Flexoki Dark"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 10.5
+
+config.background = {
+  {
+    source = {
+      File = "/home/lalit/.config/wallpapers/outerwilds-dark.jpg",
+    },
+    hsb = {
+      hue = 1.0,
+      saturation = 1.0,
+      brightness = 0.4,
+    },
+    width = "Cover",
+    height = "Cover",
+    opacity = 1,
+  },
+}
+
+config.window_padding = {
+  left = 5,
+  right = 5,
+  bottom = 0,
+  top = 0,
+}
+
+return config
---
+++ zsh/.zshrc
@@ -0,0 +1,85 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# setup zinit home
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+if [ ! -d "$ZINIT_HOME" ]; then
+	mkdir -p "$(dirname $ZINIT_HOME)"
+	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+source "${ZINIT_HOME}/zinit.zsh"
+
+# p10k install
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# add zsh plugins
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#c0c0c0'
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+autoload -U compinit && compinit
+
+# p10k source
+[[ ! -f "$ZDOTDIR/.p10k.zsh" ]] || source "$ZDOTDIR/.p10k.zsh"
+
+# keybindings
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+# history
+HISTSIZE=10000
+HISTFILE="$ZDOTDIR/.zsh_history"
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups 
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# completion sytling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath' 
+
+# aliases
+alias ls='ls --color'
+
+alias neko='echo miao'
+alias nix-rebuild='$HOME/dotfiles/rebuild.sh'
+
+alias gd='git diff'
+alias ga='git add'
+alias gc='git commit'
+alias gu='git push'
+alias gd='git pull'
+alias gs='git status'
+alias gi='git init'
+alias gcl='git clone'
+
+alias cat="bat"
+alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+alias tmux='tmux -f ~/.config/tmux/.tmux.conf'
+alias tn='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias ta='tmux attach-session'
+alias tk='tmux kill-session'
+
+# path
+export PATH="$PATH:$HOME/.cargo/bin:"
+. "$HOME/.cargo/env"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+eval "$(zoxide init zsh)"
